97cc6f6e430ec7696e0017d7da2ced8be07fae90
TylerOliverTaylor <61921198+TylerOliverTaylor@users.noreply.github.com>
2020-05-28 19:05:39 -0400

    sayGoodbye challenge

--- index.js
+++ index.js
@@ -36,8 +36,9 @@ function addNumbers(num1, num2) {
  * the returned value should look like: 'Goodbye, Andy. Have a great day.'
  * 
 */
-function sayGoodbye(/* code here */) {
-  /* code here */
+function sayGoodbye(N) {
+  var name = N;
+  console.log("Goodbye,", name, ". Have a great day.");
 }
 
 /**
